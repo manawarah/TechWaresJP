54b32fbbf90a92b6442281030ae89633d8f6ed89
Guilherme <129104268+Guilherme0112@users.noreply.github.com>
2024-05-07 10:38:56 -0300

    Add files via upload

---
+++ E-commerce/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/default.css">
+    <title>TechWareJP</title>
+</head>
+<body>
+    <header>
+        <input type="search" name="" class="search placeholder-center" placeholder="Pesquisar">
+    </header>
+</body>
+</html>
