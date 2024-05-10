84d80c3e1daed6606c8bd7f809824f8110475f6a
Guilherme <129104268+Guilherme0112@users.noreply.github.com>
2024-05-10 19:34:47 -0300

    Add files via upload

--- E-commerce/index.php
+++ E-commerce/index.php
@@ -1,15 +1,69 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="styles/default.css">
+    <link rel="stylesheet" href="styles.css">
+    <!-- O arquivo default.css serve para guardar estilos que serão usados várias vezes em outras páginas -->
+    <link rel="stylesheet" href="styles/defaults.css">
     <title>TechWareJP</title>
 </head>
 <body>
     <header>
         <input type="search" name="" class="search placeholder-center" placeholder="Pesquisar">
     </header>
+    <main>
+
+<!-- Background de entrada -->
+
+        <figure>
+            <img src="./assets/photos/back-main.png" alt="Banckgound Principal" class="background-main">
+        </figure>
+
+<!-- Caixas de promoção -->
+
+        <div id='promo'>
+            <div class="box-promo">
+                <img src="./assets/photos/products/monitor_1.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Monitor</a>
+                <button class="btn-promo">R$300,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/monitor_2.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Monitor</a>
+                <button class="btn-promo">R$700,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/cpu_1.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">CPU</a>
+                <button class="btn-promo">R$3300,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/mouse_1.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Mouse</a>
+                <button class="btn-promo">R$150,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/teclado_1.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Teclado</a>
+                <button class="btn-promo">R$100,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/teclado_2.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Teclado</a>
+                <button class="btn-promo">R$210,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/cpu_2.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">CPU</a>
+                <button class="btn-promo">R$150,00</button>
+            </div>
+            <div class="box-promo">
+                <img src="./assets/photos/products/fone_1.png" alt="" class="img-promo">
+                <a href='#' class="desc-promo">Fone de Ouvido</a>
+                <button class="btn-promo">R$50,00</button>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
