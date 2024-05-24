086d17525a5831b72628c1f0251194dbf054354a
Guilherme <129104268+Guilherme0112@users.noreply.github.com>
2024-05-24 11:50:54 -0300

    Add files via upload

--- E-commerce/product/product.php
+++ E-commerce/product/product.php
@@ -1,7 +1,7 @@
 <?php
     include_once('../database/conexao.php');
     session_start();
-    if(isset($_SESSION['email']) == true){
+    if(isset($_SESSION['email']) == false){
 
         header('Location: ../login/login.php');
     }
@@ -21,9 +21,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="product.css">
     <link rel="stylesheet" href="../styles/defaults.css">
-    <title><?php echo "$pName" ?></title>
+    <title><?php echo $pName ?></title>
 </head>
 <body>
-    
+    <main>
+        <div style="width: 230px;">
+            <img src="<?php echo "../$pPhoto" ?>" alt="">
+            <h2>R$ <?php echo $pPrice ?></h2>
+        </div>
+        <div>
+            <h1><?php echo $pName ?></h1>
+            <input type="text" name="" placeholder="Digite o nome do cartão *">
+            <input type="number" name="" placeholder="Digite o número do cartão *">
+            <input type="number" name="" placeholder="MM / AA *" style="width: 150px;">
+            <input type="number" name="" placeholder="CVV *" style="width: 150px;">
+            <select name="" id="">
+                <option value="">1x no cartão sem juros</option>
+                <option value="">2x no cartão sem juros</option>
+                <option value="">3x no cartão sem juros</option>
+                <option value="">4x no cartão sem juros</option>
+            </select>
+        </div>
+        <input type="submit" value="Comprar" name="submit">
+    </main>
 </body>
 </html>
